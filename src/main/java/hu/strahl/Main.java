c482fe070d35365d7d9246475bd2bea00597768b
Stráhl István <strahlistvan0@gmail.com>
2025-01-04 23:25:00 +0100

    initial commit

---
+++ src/main/java/hu/strahl/Main.java
@@ -0,0 +1,9 @@
+package hu.strahl;
+
+import hu.strahl.util.CrawlerUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(CrawlerUtil.findRandomEbayItem());
+    }
+}
